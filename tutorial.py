8b80592c62bdf73bc44eb663dbd6adf6d2466aeb
Will Davis <willdavis@Wills-MacBook-Pro.local>
2024-01-09 05:12:40 -0500

    Added Sprite processing

--- tutorial.py
+++ tutorial.py
@@ -4,16 +4,99 @@ import math
 import pygame
 from os import listdir
 from os.path import isfile, join
+from pygame import sprite
+
 
 pygame.init()
 pygame.display.set_caption("Platformer")
 
 WIDTH, HEIGHT = 1000, 800
-
 FPS = 60
 PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path)]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+
+    return all_sprites
+
+
+class Player(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+    GRAVITY = 1
+    SPRITES = load_sprite_sheets("MainCharacters", "NinjaFrog", 32, 32, True)
+    ANIMATION_DELAY = 3
+
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.Mask = None
+        self.direction = "left"
+        self.animation_count = 0
+        self.fall_count = 0
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = (self.animation_count //
+                        self.ANIMATION_DELAY) % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+
+    def loop(self, fps):
+        # self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY) # GRAVITY code
+        self.move(self.x_vel, self.y_vel)
+        self.fall_count += 1
+        self.update_sprite()
+
+    def draw(self, win):
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
 def get_background(name):
@@ -30,15 +113,30 @@ def get_background(name):
     return tiles, image
 
 
-def draw(window, background, bg_image):
+def draw(window, background, bg_image, player):
     for tile in background:
         window.blit(bg_image, tile)
+
+    player.draw(window)
+
     pygame.display.update()
+
+
+def handle_move(player):
+    keys = pygame.key.get_pressed()
+
+    player.x_vel = 0
+    if keys[pygame.K_LEFT]:
+        player.move_left(PLAYER_VEL)
+    if keys[pygame.K_RIGHT]:
+        player.move_right(PLAYER_VEL)
 
 
 def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("Blue.png")
+
+    player = Player(100, 100, 50, 50)
 
     run = True
     while run:
@@ -48,7 +146,9 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
-        draw(window, background, bg_image)
+        player.loop(FPS)
+        handle_move(player)
+        draw(window, background, bg_image, player)
     pygame.quit()
     quit()
 
@@ -56,4 +156,4 @@ def main(window):
 if __name__ == "__main__":
     main(window)
 
-# https://www.youtube.com/watch?v=6gLeplbqtqg&t=658s
+# https://www.youtube.com/watch?v=6gLeplbqtqg
